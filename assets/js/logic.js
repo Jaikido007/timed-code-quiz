588da138d37a9db9742937bb16895f88f330b8fe
jaikido007 <jaymullen@gmail.com>
2023-01-23 22:50:37 +0000

    Setting up all required elements in JavaScript logic file

---
+++ assets/js/logic.js
@@ -0,0 +1,11 @@
+// Getting all required elements
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = document.querySelector(".buttons .quit");
+const continue_btn = document.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+const option_list = document.querySelector(".options_list");
+const time_line = document.querySelector("header .time_line");
+const time_text = document.querySelector(".timer .time_text");
+const time_count = document.querySelector(".timer .timer_sec");
