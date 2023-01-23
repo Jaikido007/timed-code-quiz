4ab71ed83b46a31903548336d65395c3727e8cf5
jaikido007 <jaymullen@gmail.com>
2023-01-23 23:02:24 +0000

    Adding functionality to the start button and quit button by adding or removing a new class called activeInfo which reveals or hides the display box onClick

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -9,3 +9,14 @@ const option_list = document.querySelector(".options_list");
 const time_line = document.querySelector("header .time_line");
 const time_text = document.querySelector(".timer .time_text");
 const time_count = document.querySelector(".timer .timer_sec");
+
+// If start quiz button clicked
+start_btn.onclick = () => {
+	info_box.classList.add("activeInfo"); // show the info box
+    console.log('clicked')
+};
+
+// If exit quiz button clicked
+exit_btn.onclick = () => {
+	info_box.classList.remove("activeInfo"); // hide the info box
+};
